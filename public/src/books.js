9c9346b865f05b9b48bd33a1ef2f0c7de7982dff
Saivon Schenck <schencksaivon@Saivons-Air.hsd1.ga.comcast.net>
2022-06-22 09:41:18 -0400

    Added the findBookById Function

--- public/src/books.js
+++ public/src/books.js
@@ -2,11 +2,23 @@ function findAuthorById(authors, id) {
   return authors.find((author) => author.id === id)
   }
   
-  function findBookById(books, id) {}
+  function findBookById(books, id) {
+
+    for (let index = 0; index <books.length; index++) {
+      const element = books[index];
+      if(books[index].id === id) {
+        return element
+      }
+    } 
+  }
   
-  function partitionBooksByBorrowedStatus(books) {}
+  function partitionBooksByBorrowedStatus(books) {
+
+  }
   
-  function getBorrowersForBook(book, accounts) {}
+  function getBorrowersForBook(book, accounts) {
+
+  }
   
   module.exports = {
     findAuthorById,
